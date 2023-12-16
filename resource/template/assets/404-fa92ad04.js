580a2004e30f4566c703640afdd08b129d5e7e3a
zj <mjz>
2023-12-16 09:01:54 +0700

    5

---
+++ resource/template/assets/404-fa92ad04.js
@@ -0,0 +1,1 @@
+import{_ as e,b as t,m as c}from"./index-995db30f.js";const s={},n={class:"text-center",style:{"font-size":"130px"}};function o(r,_){return t(),c("div",n,"404")}const f=e(s,[["render",o]]);export{f as default};
