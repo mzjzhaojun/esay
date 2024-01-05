49410b94ab3efab677d6d1fe591e515325c168e5
zj <mjz>
2024-01-05 16:22:31 +0700

    1.5.1

--- resource/template/assets/404-1805357e.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,b as t,m as c}from"./index-fc37d5dd.js";const s={},n={class:"text-center",style:{"font-size":"130px"}};function o(r,_){return t(),c("div",n,"404")}const f=e(s,[["render",o]]);export{f as default};
