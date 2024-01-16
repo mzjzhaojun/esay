b5f34e5659a09deb777946d2f7e1fc8c121fdb81
zj <mjz>
2024-01-16 20:00:42 +0700

    1-16

---
+++ resource/template/assets/404-28bbb85e.js
@@ -0,0 +1,1 @@
+import{_ as e,b as t,m as c}from"./index-971bc216.js";const s={},n={class:"text-center",style:{"font-size":"130px"}};function o(r,_){return t(),c("div",n,"404")}const f=e(s,[["render",o]]);export{f as default};
