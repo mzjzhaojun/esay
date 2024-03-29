9c79e4fed7cb1837fa62249dca4797879e27ae4e
zj <mjz>
2024-03-29 17:34:38 +0700

    1

--- src/main/java/com/yt/app/common/common/YtGenericFilter.java
+++ src/main/java/com/yt/app/common/common/YtGenericFilter.java
@@ -35,11 +35,14 @@ public class YtGenericFilter extends GenericFilterBean {
 		localHttpServletResponse.setHeader("Access-Control-Allow-Origin",
 				(String) this.ytconfig.getOrigins().get(localInteger.intValue()));
 		localHttpServletResponse.setHeader("Access-Control-Allow-Credentials", "true");
-		localHttpServletResponse.setHeader("Access-Control-Allow-Methods", "DELETE, GET, OPTIONS, PATCH, POST, PUT");
+		localHttpServletResponse.setHeader("Access-Control-Allow-Methods", "DELETE, GET, OPTIONS, POST, PUT");
 		localHttpServletResponse.setHeader("Access-Control-Max-Age", "3600");
 		localHttpServletResponse.setHeader("Access-Control-Allow-Headers",
 				"X-Requested-With,Content-Type,Tenant_Id,Tenant_Flag,Pagenum,Pagesize,OrderBy,Dir,Satoken,Signa");
 		localHttpServletResponse.setHeader("Access-Control-Expose-Headers", "Signa,Sign");
+		localHttpServletResponse.setHeader("X-XSS-Protection", "1; mode=block");
+		localHttpServletResponse.setHeader("Content-Security-Policy", "script-src * 'unsafe-inline' 'unsafe-eval'");
+		localHttpServletResponse.setHeader("X-Content-Type-Options", "nosniff");
 		localHttpServletResponse.setHeader("X-Frame-Options", "SAMEORIGIN");
 		paramFilterChain.doFilter(paramServletRequest, paramServletResponse);
 	}
