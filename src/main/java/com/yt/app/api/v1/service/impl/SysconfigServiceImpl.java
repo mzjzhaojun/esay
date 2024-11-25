96a4721c3d7112711bcc2b372896434c58b66541
zj <mjz>
2024-11-25 11:11:43 +0800

    1

--- src/main/java/com/yt/app/api/v1/service/impl/SysconfigServiceImpl.java
+++ src/main/java/com/yt/app/api/v1/service/impl/SysconfigServiceImpl.java
@@ -78,16 +78,6 @@ public class SysconfigServiceImpl extends YtBaseServiceImpl<Sysconfig, Long> imp
 	private void getUsdtExchange() {
 		HttpHeaders headers = new HttpHeaders();
 		headers.add("user-agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36");
-		headers.add("sec-ch-ua", "Google Chrome\";v=\"131\", \"Chromium\";v=\"131\", \"Not_A Brand\";v=\"24");
-		headers.add("sec-ch-ua-platform", "macOS");
-		headers.add("service-worker-navigation-preload", "true");
-		headers.add("sec-ch-ua-mobile", "?0");
-		headers.add("sec-fetch-dest", "document");
-		headers.add("sec-fetch-mode", "navigate");
-		headers.add("sec-fetch-site", "none");
-		headers.add("sec-fetch-user", "?1");
-		headers.add("upgrade-insecure-requests", "1");
-		headers.add("accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7");
 		HttpEntity<Object> httpEntity = new HttpEntity<Object>(headers);
 		RestTemplate resttemplate = new RestTemplate();
 		ResponseEntity<SysOxxVo> sov = resttemplate.exchange("https://www.okx.com/v3/c2c/tradingOrders/books?quoteCurrency=CNY&baseCurrency=USDT&side=sell&paymentMethod=all&userType=all&receivingAds=false", HttpMethod.GET, httpEntity,
