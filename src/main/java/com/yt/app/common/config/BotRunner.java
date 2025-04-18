d6d9bcc78b7aac452e31366d98f1a706769393cd
zj <mjz>
2025-04-18 21:14:46 +0800

    修改代码提交错误页面问题

--- src/main/java/com/yt/app/common/config/BotRunner.java
+++ src/main/java/com/yt/app/common/config/BotRunner.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
@@ -24,6 +25,7 @@ import com.yt.app.common.bot.TronBot;
  * @date 2020/5/22 19:29
  */
 @Slf4j
+@Profile("master")
 @Component
 public class BotRunner implements CommandLineRunner {
 
